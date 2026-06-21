5ea6509c78affb712c9af129f9469adf04e5f7e8
vparenti867-cell <vparenti867@gmail.com>
2026-06-20 19:46:38 -0500

    Update link in HeroSection component

--- src/components/gifted/HeroSection.jsx
+++ src/components/gifted/HeroSection.jsx
@@ -46,7 +46,7 @@ export default function HeroSection() {
           Masterpieces in Progress
         </p>
         <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSdgGTrLBpw8_2n_uY8ugW73zEIhMmaKMgixHRBUOiAxgQKqxA/viewform?usp=sharing&ouid=104027221578412199654"
+          href="https://forms.gle/5fZA7cy4Aj5SDEEA9"
           className="inline-block bg-[#2d1548] text-white font-body text-sm tracking-[0.15em] px-8 py-3 hover:bg-[#3d1f60] transition-colors border border-[#c9a84c]/40"
         >
           JOIN THE TEAM
